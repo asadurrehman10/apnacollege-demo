b5fe86a6016e7267d430ec985a6d1882d6123c38
asadurrehman10 <asad03414332891@gmail.com>
2025-04-19 00:38:20 +0500

    add python api

--- new.py
+++ new.py
@@ -1,1 +1,16 @@
-print('Hello world')
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-e214b709f70e434492788cc706e80764", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
